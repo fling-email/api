64c42f1c0d4f035b565f7e8ca31265fcb2f941ab
Jacek Kuzemczak <jacek@kuzemcak.co.uk>
2021-01-03 18:19:13 +0000

    Added missing union type annotations

--- app/Exceptions/AppException.php
+++ app/Exceptions/AppException.php
@@ -8,13 +8,22 @@ use Symfony\Component\HttpFoundation\Response as SymfonyResponse;
 
 abstract class AppException extends \Exception
 {
+    /**
+     * @var string|null $debug
+     */
     protected ?string $debug;
+
+    /**
+     * @var array|null
+     * @phan-var array<string, mixed>|null
+     */
     protected ?array $data;
 
     /**
      * @param string $message The error message
      * @param string|null $debug More debug info about the error
      * @param array|null $data Data to support the debug information
+     * @phan-param array<string, mixed>|null $data
      */
     public function __construct(string $message, ?string $debug = null, ?array $data = null)
     {
