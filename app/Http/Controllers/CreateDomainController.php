894de47408e00ca0b600d0d124cffd3eb46e51e7
Jacek Kuzemczak <jacek@kuzemcak.co.uk>
2021-01-26 01:43:54 +0000

    WIP: Implemented domain suffix validation fully

--- app/Http/Controllers/CreateDomainController.php
+++ app/Http/Controllers/CreateDomainController.php
@@ -32,8 +32,6 @@ class CreateDomainController extends Controller
 
         $this->validateDomainName($domain_name);
 
-        $this->validateDomainName($domain_name);
-
         $domain_already_exists = Domain::query()
             ->where("organisation_id", $org->id)
             ->where("name", $domain_name)
@@ -60,6 +58,15 @@ class CreateDomainController extends Controller
         return \response()->json($domain);
     }
 
+    /**
+     * Validates the domain name from the request
+     *
+     * @param string $domain_name The name from the request
+     *
+     * @throws BadRequestException
+     *
+     * @return void
+     */
     private function validateDomainName(string $domain_name): void
     {
         $suffix_list = Cache::remember(
@@ -71,23 +78,103 @@ class CreateDomainController extends Controller
                 return \collect(explode("\n", $raw_data))
                     ->map(fn (string $line): string => \trim($line))
                     ->filter(fn (string $line): bool => (
-                        $line === "" || Str::startsWith($line, "//")
-                    ));
+                        $line !== "" && !Str::startsWith($line, "//")
+                    ))
+                    ->toArray();
             }
         );
 
-        $valid_suffix = false;
+        $matched_suffix_rules = \collect([]);
 
         foreach ($suffix_list as $suffix) {
-            if (Str::endsWith($domain_name, ".{$suffix}")) {
-                $valid_suffix = true;
+            if ($this->domainMatchesSuffix($domain_name, $suffix)) {
+                $matched_suffix_rules->add($suffix);
+            }
+        }
+
+        // Sort by rule priority
+        $matched_suffix_rules = $matched_suffix_rules->sort(
+            function (string $a, string $b): int {
+                $a_is_exception = \substr($a, 0, 1) === "!";
+                $b_is_exception = \substr($b, 0, 1) === "!";
+
+                if ($a_is_exception && !$b_is_exception) {
+                    return -1;
+                }
+
+                if ($b_is_exception && !$a_is_exception) {
+                    return 1;
+                }
+
+                return \substr_count($b, ".") <=> \substr_count($a, ".");;
+            }
+        );
+
+        $prevailing_rule = $matched_suffix_rules->first();
+
+        if ($prevailing_rule === null) {
+            // no matches at all means a fully invalid domain
+            throw new BadRequestException("Invalid hostname at #->properties:name");
+        }
+
+        if (\substr($prevailing_rule, 0, 1) === "!") {
+            // Exception rule means invalid domain
+            throw new BadRequestException("Invalid hostname at #->properties:name");
+        }
+    }
+
+    /**
+     * Checks if a domain matches a suffix from the public suffix list
+     *
+     * @param string $domain_name The domain to check
+     * @param string $suffix The suffix to check against
+     *
+     * @return boolean
+     */
+    private function domainMatchesSuffix(string $domain_name, string $suffix): bool
+    {
+        $is_exception_rule = \substr($suffix, 0, 1) === "!";
+
+        // Stop off the ! for exception rules so we can use the same parsing
+        if ($is_exception_rule) {
+            $suffix = \substr($suffix, 1);
+        }
+
+        // Ignore case
+        $domain_name = \strtolower($domain_name);
+
+        // Matching works one "part" (or label) at a time starting from the right
+        $domain_labels = \array_reverse(\explode(".", $domain_name));
+        $suffix_labels = \array_reverse(\explode(".", $suffix));
+
+        // If the domain is shorter than the suffix it can't match
+        if (\count($domain_labels) < \count($suffix_labels)) {
+            return false;
+        }
+
+        $total_suffix_labels = \count($suffix_labels);
+
+        $matched = true;
+
+        for ($i = 0; $i < $total_suffix_labels; ++$i) {
+            $domain_label = $domain_labels[$i];
+            $suffix_label = $suffix_labels[$i];
+
+            // Needs to be a wildcard or a exact match.
+            if ($suffix_label !== "*" && $suffix_label !== $domain_label) {
+                $matched = false;
                 break;
             }
         }
 
-        if (!$valid_suffix) {
-            throw new BadRequestException("Invalid hostname at #->properties:name");
-        }
+        // Matched all if we got to the last itteration
+        $checked_all = $total_suffix_labels === $i;
+
+        return ($is_exception_rule)
+            // Exeption rules need to be a full match
+            ? $matched && $checked_all
+            // Normal rules just need to not not match
+            : $matched;
     }
 
     /**
