c31dc4359e5aec7ad40e00390b4cb7993b4c6df2
Jacek Kuzemczak <jacek@dataplan.co.uk>
2021-11-07 21:54:49 +0000

    Removed all database config except connection details from .env file

--- config/database.php
+++ config/database.php
@@ -6,23 +6,23 @@ use Illuminate\Database\DBAL\TimestampType;
 
 return [
 
-    "default" => env("DB_CONNECTION", "mysql"),
+    "default" => \env("DB_CONNECTION", "mysql"),
 
     "connections" => [
         "mysql" => [
             "driver" => "mysql",
-            "host" => env("DB_HOST", "127.0.0.1"),
-            "port" => env("DB_PORT", 3306),
-            "database" => env("DB_DATABASE", "forge"),
-            "username" => env("DB_USERNAME", "forge"),
-            "password" => env("DB_PASSWORD", ""),
-            "unix_socket" => env("DB_SOCKET", ""),
-            "charset" => env("DB_CHARSET", "utf8mb4"),
-            "collation" => env("DB_COLLATION", "utf8mb4_unicode_ci"),
-            "prefix" => env("DB_PREFIX", ""),
-            "strict" => env("DB_STRICT_MODE", true),
-            "engine" => env("DB_ENGINE", null),
-            "timezone" => env("DB_TIMEZONE", "+00:00"),
+            "host" => \env("DB_HOST", "127.0.0.1"),
+            "port" => \env("DB_PORT", 3306),
+            "database" => \env("DB_DATABASE"),
+            "username" => \env("DB_USERNAME"),
+            "password" => \env("DB_PASSWORD", ""),
+            "unix_socket" => \env("DB_SOCKET", ""),
+            "charset" => "utf8mb4",
+            "collation" => "utf8mb4_bin",
+            "prefix" => "",
+            "strict" => true,
+            "engine" => "InnoDB",
+            "timezone" => "+00:00",
         ],
     ],
 
