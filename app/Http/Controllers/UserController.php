94f633df7cf955bf3c57678b9a818b56b01f41d5
Jacek Kuzemczak <jacek@kuzemcak.co.uk>
2021-12-24 13:27:53 +0000

    Removed unused function param

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace App\Http\Controllers;
 
 use App\Models\User;
-use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Http\JsonResponse;
 
@@ -17,12 +16,11 @@ class UserController extends Controller
     /**
      * Handles requests to the /users/{uuid} endpoint
      *
-     * @param Request $request the request
      * @param string $uuid The UUID of the user
      *
-     * @return Request|JsonResponse
+     * @return Response|JsonResponse
      */
-    public function __invoke(Request $request, string $uuid): Response|JsonResponse
+    public function __invoke(string $uuid): Response|JsonResponse
     {
         $user = User::where("uuid", $uuid)->first();
 
