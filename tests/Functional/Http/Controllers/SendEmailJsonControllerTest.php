db2189a1ea2775e6aaa0b38a5c15f4e3f2682c5d
Jacek Kuzemczak <jacek@kuzemcak.co.uk>
2022-02-07 23:27:09 +0000

    Make test code a bit nicer

--- tests/Functional/Http/Controllers/SendEmailJsonControllerTest.php
+++ tests/Functional/Http/Controllers/SendEmailJsonControllerTest.php
@@ -18,6 +18,21 @@ class SendEmailJsonControllerTest extends TestCase
                        ->domains
                        ->first();
 
+        $mjml = <<<MJML
+            <mjml>
+                <mj-body>
+                    <mj-section>
+                        <mj-column>
+                            <mj-text>Hello World</mj-text>
+                        </mj-column>
+                    </mj-section>
+                </mj-body>
+            </mjml>
+            MJML;
+
+        $file_name = "cool-file.txt";
+        $file_data = "cool file data";
+
         $this->actingAsTestUser()
             ->json("POST", "/emails/json", [
                 "to" => [
@@ -33,16 +48,40 @@ class SendEmailJsonControllerTest extends TestCase
                 "from_email" => "from@{$domain->name}",
                 "subject" => "An Interesting Email",
                 "message" => [
-                    "mjml" => "<mjml><mj-body><mj-section><mj-column><mj-text>Hello World</mj-text></mj-column></mj-section></mj-body></mjml>",
+                    "mjml" => $mjml,
                 ],
                 "attachments" => [
                     [
-                        "name" => "cool-file.txt",
-                        "data" => \base64_encode("cool file data"),
+                        "name" => $file_name,
+                        "data" => \base64_encode($file_data),
                     ]
                 ],
             ])
             ->dontSeeJsonSchemaError()
-            ->seeStatusCode(201);
+            ->seeStatusCode(201)
+            ->seeInDatabase("emails", [
+                "from_name" => "From Name",
+                "from_email" => "from@{$domain->name}",
+                "subject" => "An Interesting Email",
+                "message_mjml" => $mjml,
+                "message_plain" => "Hello World",
+            ])
+            ->seeInDatabase("attachments", [
+                "name" => $file_name,
+                "type" => "text/plain",
+                "size" => \strlen($file_data),
+                "md5" => \md5($file_data),
+                "sha1" => \sha1($file_data),
+                "data" => $file_data,
+            ])
+            ->seeInDatabase("recipients", [
+                "email_address" => "to@{$domain->name}",
+            ])
+            ->seeInDatabase("recipients", [
+                "email_address" => "cc@{$domain->name}",
+            ])
+            ->seeInDatabase("recipients", [
+                "email_address" => "bcc@{$domain->name}",
+            ]);
     }
 }
